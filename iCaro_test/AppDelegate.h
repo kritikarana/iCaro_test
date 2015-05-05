99cd37ce5030fcd80b75cdfcfe1eaaa03f83da01
kritikarana <kritikarana@gmail.com>
2015-05-04 22:30:08 -0400

    Initial Commit

---
+++ iCaro_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iCaro_test
+//
+//  Created by admin on 5/4/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iCaro_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iCaro_test
+//
+//  Created by admin on 5/4/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
